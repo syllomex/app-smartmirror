345d2e3afa6082f8090597851fa454088d83ebc6
Leonardo Santos <leonardojdossantos@gmail.com>
2021-09-16 23:05:19 -0300

    chore: change api uri

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// const api = axios.create({ baseURL: 'https://api-smartmirror.herokuapp.com' });
-const api = axios.create({ baseURL: 'http://192.168.0.104:8000' });
+const api = axios.create({ baseURL: 'https://api-smartmirror.herokuapp.com' });
+// const api = axios.create({ baseURL: 'http://192.168.0.104:8000' });
 
 export { api };
