eb362bfb7c5b865dcaa7a7fd5fc9042d17e5191b
Leonardo Santos <leonardojdossantos@gmail.com>
2021-10-17 13:17:17 -0300

    feat: add button themes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,18 +6,25 @@ import { Container, Label } from './styles';
 interface Props extends TouchableOpacityProps {
   left?: React.ReactNode;
   right?: React.ReactNode;
+  theme?: 'default' | 'outline';
 }
 
-const Button: React.FC<Props> = ({ children, left, right, ...props }) => {
+const Button: React.FC<Props> = ({
+  children,
+  left,
+  right,
+  theme,
+  ...props
+}) => {
   return (
-    <Container {...props}>
+    <Container {...props} theme={theme}>
       {left}
-      <Label>{children}</Label>
+      <Label theme={theme}>{children}</Label>
       {right}
     </Container>
   );
 };
 
-Button.defaultProps = { left: null, right: null };
+Button.defaultProps = { left: null, right: null, theme: 'default' };
 
 export default Button;
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,8 +3,11 @@ import styled from 'styled-components/native';
 import { colors } from '../../assets/colors';
 import rem from '../../utils/rem';
 
-export const Container = styled.TouchableOpacity`
-  background-color: ${colors.light_1};
+export const Container = styled.TouchableOpacity<{
+  theme: 'default' | 'outline';
+}>`
+  background-color: ${({ theme }) =>
+    theme === 'default' ? colors.light_1 : 'transparent'};
   border-radius: ${rem(2)};
   padding: ${rem(1.2)};
 
@@ -12,11 +15,17 @@ export const Container = styled.TouchableOpacity`
 
   justify-content: center;
   align-items: center;
+
+  ${({ theme }) =>
+    theme === 'outline'
+      ? `border-width: 1px; border-color: ${colors.text_light_1}`
+      : ''}
 `;
 
-export const Label = styled.Text`
+export const Label = styled.Text<{ theme: 'default' | 'outline' }>`
   font-family: 'Quicksand-Regular';
-  color: ${colors.text_darK_1};
+  color: ${({ theme }) =>
+    theme === 'default' ? colors.text_darK_1 : colors.text_light_1};
   font-size: ${rem(1.4)};
 
   text-align: center;
