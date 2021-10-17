2778f420d418789e3cdf52a03fc5816bd0fc8195
Leonardo Santos <leonardojdossantos@gmail.com>
2021-10-17 13:17:01 -0300

    chore: add new colors

--- src/assets/colors/index.ts
+++ src/assets/colors/index.ts
@@ -3,8 +3,11 @@ export const colors = {
   dark_2: '#2A2E47',
   dark_3: '#747892',
   darK_1_transparent: '#0C1625AA',
+  dark_2_transparent: 'rgba(12, 22, 37, 0.4)',
   text_light_1: '#FFFFFF',
   text_darK_1: '#333333',
   light_1: '#FFFFFF',
+  light_2: '#E6E7EE',
   light_1_transparent: '#FFFFFF44',
+  danger: '#FF9999',
 };
